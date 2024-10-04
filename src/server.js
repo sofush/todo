e6bc4b1cb5f528269fe197f83ccfb9aa97c5352a
Sofus Hjort Helmer <102423577+sofush@users.noreply.github.com>
2024-10-04 15:00:06 +0200

    feat: fetch tasks from api endpoint

--- src/server.js
+++ src/server.js
@@ -2,20 +2,77 @@ const express = require('express');
 const path = require('path')
 const app = express();
 
-const port = 3000;
+app.use(express.json());
+
+var tasks = [
+    {
+        id: 1,
+        description: "Buy groceries",
+        completed: true,
+    },
+    {
+        id: 2,
+        description: "Cook dinner",
+        completed: false,
+    },
+    {
+        id: 3,
+        description: "Do the dishes",
+        completed: false,
+    },
+    {
+        id: 4,
+        description: "Work out",
+        completed: false,
+    }
+];
+var counter = tasks.length;
 
 const getFile = function(file) {
     return path.join(__dirname, '..', `static/${file}`);
 };
 
-app.get('/static/:file', (req, res) => {
+app.get('/static/:file', async (req, res) => {
     res.sendFile(getFile(req.params.file));
 });
 
-app.get('/', (_req, res) => {
+app.get('/', async (_req, res) => {
     res.sendFile(getFile('index.html'));
 });
 
+app.get('/tasks', async (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(tasks));
+});
+
+app.post('/add/:description', async (req, res) => {
+    const description = req.params.description;
+
+    if (description !== undefined) {
+        tasks.push({
+            id: ++counter,
+            description: description,
+            completed: false,
+        });
+    }
+
+    res.redirect('/');
+});
+
+app.post('/toggle/:id', async (req, res) => {
+    tasks
+        .filter(task => task.id == req.params.id)
+        .forEach(task => task.completed = !task.completed);
+    res.redirect('/');
+});
+
+app.post('/delete/:id', async (req, res) => {
+    tasks = tasks.filter(task => task.id == req.params.id);
+    res.redirect('/');
+});
+
+const port = 3000;
 app.listen(port, () => {
     console.log(`Server is listening on http://localhost:${port}`);
 });
--- static/site.js
+++ static/site.js
@@ -1,13 +1,50 @@
-document.addEventListener('DOMContentLoaded', e => {
+const addTaskElement = (task) => {
+    const container = document.getElementsByClassName('container')[0];
+    const form = container.getElementsByTagName('form')[0];
+    const taskContainer = document.createElement('div');
+    const index = document.createElement('label');
+    const description = document.createElement('label');
+    
+    taskContainer.appendChild(index);
+    taskContainer.appendChild(description);
+    container.insertBefore(taskContainer, form);
+
+    index.innerHTML = task.id + ")";
+    description.innerHTML = task.description;
+    if (task.completed === true)
+        taskContainer.classList.add('completed');
+
+    taskContainer.classList.add('task-container');
+    index.classList.add('task-index');
+    description.classList.add('task');
+};
+
+// Load tasks and insert HTML elements
+document.addEventListener('DOMContentLoaded', async e => {
+    const response = await fetch('/tasks');
+    if (!response.ok) return;
+    
+    const json = await response.json();
+
+    Array.from(json).forEach(task => {
+        addTaskElement(task);
+    });
+    
     const containers = document.getElementsByClassName('task-container');
     
     Array.from(containers).forEach(container => {
         const inner = container.getElementsByClassName('task');
+        const index = container.getElementsByClassName('task-index')[0];
         
-        if (inner.length < 1) return;
+        if (inner.length < 1 || index.length < 1) return;
 
         inner[0].addEventListener('click', _ => {
-            container.classList.toggle('completed');
+            fetch('/toggle/' + index.innerHTML.replace(')', ''), {
+                method: 'post',
+            }).then(() => container.classList.toggle('completed'));
         });
     });
 });
+
+// 
+document 
